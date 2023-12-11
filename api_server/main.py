48e024f2f438235fc4aba6004289c02131ac3a54
Beto Avila <beto0112358@gmail.com>
2023-12-11 00:27:52 -0700

    pandas and mysql working

--- api_server/main.py
+++ api_server/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
-from functions import populate_tables_init
+# from fastapi import FastAPI
+# from functions import populate_tables_init
+import pandas as pd
+from sqlalchemy import create_engine
 
-populate_tables_init()
+
+df = pd.read_csv('/tmp/data/departments.csv', header=None, names=['department_id', 'department_name'])
+conn = create_engine(url='mysql+pymysql://root:root@db:3306/app_db')
+df.to_sql(name='departments', con=conn, index=False)
+
+# populate_tables_init()
 # app = FastAPI()
 
 
--- api_server/start.sh
+++ api_server/start.sh
@@ -3,9 +3,10 @@ apt-get update && \
     apt-get upgrade -y && \
     apt-get install \
     nano
-apt-get update
-apt-get install default-jre -y
-export JAVA_HOME=/usr/bin/
+# apt-get update
+# apt-get install default-jre -y
+# export JAVA_HOME=/usr/bin/
+mv data/*.csv /tmp/data/
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install "uvicorn[standard]"
